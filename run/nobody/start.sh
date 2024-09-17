b5ccc1fcbdabd9917c9ba161c0cb6e19333b508b
binhex <megalith01@gmail.com>
2024-09-17 13:31:57 +0100

    symlink definitions to config

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/dumb-init /bin/bash
 
+# source in utils functions
+source utils.sh
+
+# symlink jacket created definitions to config folder
+symlink --src-path '/usr/lib/jackett/Definitions' --dst-path '/config/Jackett/Definitions' --link-type 'softlink'
+
 # run jackett using .net core
 /usr/lib/jackett/jackett --NoRestart --NoUpdates --DataFolder /config/Jackett
